fda10aafa651fce652175cd100f644e3afcc6021
kumarshivam01 <98182315+kumarshivam01@users.noreply.github.com>
2022-04-11 07:55:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+import { useState } from "react";
+// import { Category } from "./components/Category"
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleChange = (value) => {
+    setCounter(counter + value);
+  };
+  const multChange = (value) => {
+    setCounter(counter * value);
+  };
+
+  return (
+    <div className="App">
+      <h1 className={counter % 2 === 0 ? "counterEven" : "counterOdd"}>
+        Counter:{counter}
+      </h1>
+      <button onClick={() => handleChange(1)}> Add 1</button>
+      <button onClick={() => handleChange(-1)}> Sub 1</button>
+      <button onClick={() => multChange(2)}> Double</button>
+    </div>
+  );
+}
